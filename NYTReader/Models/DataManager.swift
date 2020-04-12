6d741a5e3f38f5a8d2f6cd5a377e7a2286a3ffde
Tambanco <a.tambanco@gmail.com>
2020-04-12 23:55:10 +0300

    constructing dataManager

---
+++ NYTReader/Models/DataManager.swift
@@ -0,0 +1,21 @@
+//
+//  NewsManager.swift
+//  NYTReader
+//
+//  Created by Tambanco on 12.04.2020.
+//  Copyright © 2020 Tambanco. All rights reserved.
+//
+
+import Foundation
+
+
+struct DataManager{
+    
+    let dataURL = "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=2AY5aYQX2U3y4ytAuiNg7N6u9AMrsPpg"
+    
+}
+
+func performRequest(urlString: String){
+    
+}
+}
--- NYTReader/NewsViewController.swift
+++ NYTReader/NewsViewController.swift
@@ -15,7 +15,6 @@ class NewsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
