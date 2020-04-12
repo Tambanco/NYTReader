a2f14c443317dc8d6421a27d7a41bf9f7f8b9206
Tambanco <a.tambanco@gmail.com>
2020-04-12 18:53:29 +0300

    added init TableViewController

---
+++ NYTReader/NewsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  NYTReader
+//
+//  Created by Tambanco on 12.04.2020.
+//  Copyright © 2020 Tambanco. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UITableViewController {
+
+    let itemArray = ["news1", "news2", "news3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+}
+ 
