136b2465e66031767d922e4d5da8441d5dfd329c
Tambanco <a.tambanco@gmail.com>
2020-04-28 23:48:10 +0300

    changed sideMenu to Settings

--- NYTReader/Controller/MenuViewController.swift
+++ NYTReader/Controller/MenuViewController.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2020 Tambanco. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+           
+//        view.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+//        navigationController?.navigationBar.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+}
+   
+    @IBAction func togglePressed(_ sender: UISwitch) {
+        
+        if sender.isOn == true{
+            self.overrideUserInterfaceStyle = .dark
+            
+        }else{
+            self.overrideUserInterfaceStyle = .light
+        }
+        
+    }
+    
+}
