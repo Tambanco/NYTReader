7774a52b42072c677bd3d5685ae7ee2bfe4d0a65
Al <alexunder_5542@mail.ru>
2021-05-04 10:26:58 +0300

    added SectionsViewController with XIB

---
+++ NYTReader/Controller/XIB/SectionsTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SectionsTableViewCell.swift
+//  NYTReader
+//
+//  Created by tambanco 🥳 on 04.05.2021.
+//  Copyright © 2021 Tambanco. All rights reserved.
+//
+
+import UIKit
+
+class SectionsTableViewCell: UITableViewCell
+{
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
