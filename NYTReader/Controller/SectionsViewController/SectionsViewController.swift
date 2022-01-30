b92165d6b29246e9656af9f855ed50651a9392dc
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-30 09:36:54 +0300

    Update SectionsViewController.swift

--- NYTReader/Controller/SectionsViewController/SectionsViewController.swift
+++ NYTReader/Controller/SectionsViewController/SectionsViewController.swift
@@ -8,53 +8,53 @@
 
 import UIKit
 
-class SectionsViewController: UITableViewController {
+class SectionsViewController: UIViewController {
     // MARK: - Properties
     @IBOutlet weak var tableSections: UITableView!
     let sections = Sections()
 
     // MARK: - Life cycle
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
         
+        initializationTableSections()
+                
+    }
+    
+    func initializationTableSections() {
         tableSections.register(SectionCell.self, forCellReuseIdentifier: SectionCell.reuseId)
+        
+        self.view.addSubview(tableSections)
         
         tableSections.dataSource = self
         tableSections.delegate = self
-                
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToArticles" {
+            if let indexPath = self.tableSections.indexPathForSelectedRow {
+                let articleVC = segue.destination as! NewsViewController
+                articleVC.section = sections.capitalizeFirstCharacter(sections.splitArray(sections.allSections))[indexPath.row]
+                articleVC.navigationItem.title = sections.capitalizeFirstCharacter(sections.splitArray(sections.allSections))[indexPath.row]
+            }
+        }
+    }
+}
 
-    // MARK: - Table view data source
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
+extension SectionsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sections.capitalizeFirstCharacter(sections.splitArray(sections.allSections)).count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: SectionCell.reuseId, for: indexPath) as! SectionCell
         cell.sectionLabel.text = sections.capitalizeFirstCharacter(sections.splitArray(sections.allSections))[indexPath.row]
         cell.accessoryType = .disclosureIndicator
         return cell
     }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.performSegue(withIdentifier: "goToArticles", sender: indexPath)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-    {
-        if segue.identifier == "goToArticles"
-        {
-            if let indexPath = self.tableView.indexPathForSelectedRow
-            {
-                let articleVC = segue.destination as! NewsViewController
-                articleVC.section = sections.capitalizeFirstCharacter(sections.splitArray(sections.allSections))[indexPath.row]
-                articleVC.navigationItem.title = sections.capitalizeFirstCharacter(sections.splitArray(sections.allSections))[indexPath.row]
-            }
-        }
     }
 }
